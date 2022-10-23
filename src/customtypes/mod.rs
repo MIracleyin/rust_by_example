1b5ec7a8ee33f42debd3cf70b5e17702b01418aa
MIracleyin <miracleyin@live.com>
2022-10-23 22:45:48 +0800

    linked list

--- src/customtypes/mod.rs
+++ src/customtypes/mod.rs
@@ -1,6 +1,8 @@
 mod structs;
 mod enums;
 mod enum_use;
+mod c_like;
+mod linked_list;
 
 #[cfg(test)]
 mod customtypes_ground {
@@ -20,5 +22,15 @@ mod customtypes_ground {
     #[test]
     fn test_enums_use() {
         enum_use::enum_use();
+    }
+
+    #[test]
+    fn test_c_like() {
+        c_like::c_like()
+    }
+
+    #[test]
+    fn test_linked_list() {
+        linked_list::linked_list();
     }
 }

a5eacd04bfb05140461c99c86a75d2c7bbae1ff4
MIracleyin <miracleyin@live.com>
2022-10-24 13:59:26 +0800

    flow control for

---
+++ src/flow_control/fors.rs
@@ -0,0 +1,22 @@
+pub fn fors() {
+    // `n` 将在每次迭代中分别取 1, 2, ..., 100
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter_mut() {
+        match name {
+            &mut "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+}

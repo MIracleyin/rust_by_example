d9d15d5371958ba27ffddc1d09ba1bbf433170f8
MIracleyin <miracleyin@live.com>
2022-10-24 13:13:27 +0800

    flow control loop

---
+++ src/flow_control/loops.rs
@@ -0,0 +1,66 @@
+fn simple_loop() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    // 无限循环
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // 跳过这次迭代的剩下内容
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            // 退出循环
+            break;
+        }
+    }
+}
+
+fn nested_loop() {
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // 这只是中断内部的循环
+            //break;
+
+            // 这会中断外层循环
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
+
+fn return_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+}
+
+pub fn loops() {
+    // simple_loop();
+    // nested_loop();
+    return_loop();
+}
--- src/flow_control/mod.rs
+++ src/flow_control/mod.rs
@@ -1,11 +1,18 @@
 mod if_else;
+mod loops;
+mod whiles;
 
 #[cfg(test)]
 mod flow_control_ground {
     use super::*;
 
     #[test]
-    fn expansion() {
+    fn test_if_else() {
         if_else::if_else();
+    }
+
+    #[test]
+    fn test_loops() {
+        loops::loops();
     }
 }
